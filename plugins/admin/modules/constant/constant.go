337f3ca7d267bb1442e863628b2683294199333f
backyio <backystart@gmail.com>
2023-08-25 08:00:03 +0200

    removed additional pk

--- plugins/admin/modules/constant/constant.go
+++ plugins/admin/modules/constant/constant.go
@@ -11,16 +11,9 @@ const (
 	// PjaxUrlHeader is default pjax url http header key.
 	PjaxUrlHeader = constant.PjaxUrlHeader
 
-	EditPKKey  = "__admin_edit_pk"
-	EditPKKey1 = "__admin_edit_pk1"
-	EditPKKey2 = "__admin_edit_pk2"
-
-	DetailPKKey  = "__admin_detail_pk"
-	DetailPKKey1 = "__admin_detail_pk1"
-	DetailPKKey2 = "__admin_detail_pk2"
-
-	PrefixKey = "__prefix"
-
+	EditPKKey   = "__admin_edit_pk"
+	DetailPKKey = "__admin_detail_pk"
+	PrefixKey   = "__prefix"
 	IframeKey   = "__admin_iframe"
 	IframeIDKey = "__admin_iframe_id"
 
